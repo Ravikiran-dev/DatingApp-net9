254f4df65546ad51268a0be4d7e6ea939f36b4ad
Ravi Kiran <ravikiransankathali@gmail.com>
2025-05-06 13:05:42 +0530

    End of section 10

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTO;
 using API.Entity;
@@ -12,7 +13,7 @@ namespace API.Controllers
 {
     [Authorize]
     [Route("api/[controller]")] // /api/users
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
 
         // Add your actions here
@@ -36,5 +37,34 @@ namespace API.Controllers
 
             return user; // Map the user to MemberDto and return it  
         }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // Get the username from the token
+            if(userName == null)
+            {
+                return BadRequest("No username found in Token"); // Return 401 if user is not authorized
+            }
+
+            var user = await userRepository.GetUserByUsernameAsync(userName); // Fetch user by username from the database
+            if (user == null)
+            {
+                return NotFound("Could not find users"); // Return 404 if user not found
+            }
+
+            mapper.Map(memberUpdateDto, user); // Map the updated data to the user entity
+
+            userRepository.Update(user); // Update the user in the repository
+
+            if(await userRepository.SaveAllAsync())
+            {
+                return NoContent(); // Return 204 No Content if update is successful
+            }
+
+            return BadRequest("Failed to update user"); // Return 400 Bad Request if update fails
+
+        }
+            
     }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,25 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
 
   private accountService = inject(AccountService);
-  
+
   ngOnInit(): void {
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const userString = localStorage.getItem('user');
-    if(!userString){
+    if (!userString) {
       return;
     }
     const user = JSON.parse(userString);
@@ -29,5 +30,5 @@ export class AppComponent implements OnInit {
 
   }
 
-  
+
 }
